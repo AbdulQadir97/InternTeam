e6e707e9a7b7837c2be53de3474d553fd28cd9d6
AbdulQadir97 <aq201541@gmail.com>
2021-11-26 21:08:52 +0500

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/antd.css';
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
